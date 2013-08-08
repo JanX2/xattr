a2ef4b046c38ace995b37e20bc20f7a929233617
Jan <github@geheimwerk.de>
2013-08-08 16:40:24 +0200

    Fixing crash when adding a new entry.

--- Row.m
+++ Row.m
@@ -99,7 +99,8 @@ static NSString * convertPlistRootToXMLString(id plist) {
 // Adapted from http://stackoverflow.com/a/4771038
 static BOOL startsWith(const char *pre, const char *str)
 {
-    size_t lenpre = strlen(pre),
+    if (str == NULL)  return NO;
+	size_t lenpre = strlen(pre),
 	lenstr = strlen(str);
     return (lenstr < lenpre) ? false : (strncmp(pre, str, lenpre) == 0);
 }
